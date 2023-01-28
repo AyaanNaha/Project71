2e1b3a2953fe26c2e9ba6c28c583d63e05346544
AyaanNaha <ayaan.naha26@gmail.com>
2023-01-28 15:50:55 -0800

    added sdk to config.js

--- config.js
+++ config.js
@@ -2,6 +2,16 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKJkTzJPh-T_79XLGWdxagAeQR2nNpsZQ",
+  authDomain: "e-ride-a53ea.firebaseapp.com",
+  projectId: "e-ride-a53ea",
+  storageBucket: "e-ride-a53ea.appspot.com",
+  messagingSenderId: "808284902790",
+  appId: "1:808284902790:web:41c60dc4d5d94739044807"
+};
+
 
 //
 firebase.initializeApp(firebaseConfig);
